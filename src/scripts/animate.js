a6a2c8892a5a96e8bbf094fcda97ed1c4e099de2
larenasss <ab-vis@mail.ru>
2019-12-23 20:17:44 +0700

    Небольшие правки

--- src/scripts/animate.js
+++ src/scripts/animate.js
@@ -4,73 +4,63 @@
 import { default as anime } from '../../node_modules/animejs/lib/anime.js';
 import { delay } from 'q';
 
+$(document).ready(function(){
+
 // Анимация лыжник
 
-jQuery(function($){
-
-  $(document).ready(function(){
-    let skierAnimate = anime({
-      targets: '.event__animate',
-      left: '-192px',
-      bottom: '-65px',
-      easing: 'linear',
-      duration: 4000,
-      delay: 2000,
-      loop: true
-    })
-  }); 
+let skierAnimate = anime({
+  targets: '.event__animate',
+  left: '-192px',
+  bottom: '-65px',
+  easing: 'linear',
+  duration: 4000,
+  delay: 2000,
+  loop: true
 })
 
 // Анимация движущегося мужчины
 
-jQuery(function($){
-
-  $(document).ready(function(){
-    let myTimeline = anime.timeline({
-      update: function(anim) {
-        let result = +Math.round(anim.progress);
-        
-        if(result >= 50 ){
-          $('.counter__animate').css({transform: 'rotateY(180deg)'});
-        } else {
-          $('.counter__animate').css({transform: 'rotateY(360deg)'});
-        }
-        
-      },
-      loop: true,
-      easing: 'linear',
-      duration: 5000,
-      targets: '.counter__animate',
-    });
-
-    myTimeline
-    .add({
-      
-      left: '80%',
-      loop: true,
-    })
-    .add({
-     
-      left: '0',
-      loop: true,
-    })
-  });
-
-  // Счетчик на табличке
-
-  $(document).ready(function(){
-    let metr = $('.counter__info-metr');
-    let call = $('.counter__info-call');
+let myTimeline = anime.timeline({
+  update: function(anim) {
+    let result = +Math.round(anim.progress);
     
+    if(result >= 50 ){
+      $('.counter__animate').css({transform: 'rotateY(180deg)'});
+    } else {
+      $('.counter__animate').css({transform: 'rotateY(360deg)'});
+    }
+    
+  },
+  loop: true,
+  easing: 'linear',
+  duration: 5000,
+  targets: '.counter__animate',
+});
 
-    setInterval(function (){
-        metr.text(parseInt(metr.text())+1);
-        call.text(Number(parseFloat(call.text())+0.087).toFixed(2));
-    }, 1000);
+myTimeline
+  .add({
+    left: '80%',
+    loop: true,
+  })
+  .add({
+    left: '0',
+    loop: true,
   })
 
-    
 
-})
+// Счетчик на табличке
+
+let metr = $('.counter__info-metr');
+let call = $('.counter__info-call');
+
+
+setInterval(function (){
+    metr.text(parseInt(metr.text())+1);
+    call.text(Number(parseFloat(call.text())+0.087).toFixed(2));
+}, 1000);
+}); 
+
+
+
 
 
--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,14 +1,13 @@
 // Использована библиотека Slick
 // https://kenwheeler.github.io/slick/
 
-jQuery(function($){
-  $(document).ready(function(){
-    $('.slider__list').slick({
-      prevArrow: '.slider__prev',
-      nextArrow: '.slider__next',
-      speed: 600,
-      autoplay: true,
-      autoplaySpeed: 4000,
-    });
+
+$(document).ready(function(){
+  $('.slider__list').slick({
+    prevArrow: '.slider__prev',
+    nextArrow: '.slider__next',
+    speed: 600,
+    autoplay: true,
+    autoplaySpeed: 4000,
   });
-})
+});
