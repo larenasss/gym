7151520e2de7714d968fb81f1f76cec4261d97e9
larenasss <ab-vis@mail.ru>
2020-04-14 11:57:13 +0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/test_one/dist/';
+  const publicPath = '/gym/dist/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
